c6aaf8409524afed74702db4d283c4bbc142462c
Karin Ohana <karinohana@Karins-Air.attlocal.net>
2022-08-25 14:03:07 -0400

    app.py

--- src/app.py
+++ src/app.py
@@ -165,5 +165,5 @@ def handle_get_posts():
 
 
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=False)
